ffc85f51a39f656c1907742635476134ecb78f7e
mirexan <mmregada@gmail.com>
2025-09-22 13:52:02 +0200

    modifico la estructura del lvl1

---
+++ Level_01/src/lvl1_exercise_01/Lvl1_Ex01_Main.java
@@ -0,0 +1,10 @@
+package lvl1_exercise_01;
+
+import lvl1_exercise_01.classes.Iniciar;
+
+public class Lvl1_Ex01_Main {
+	public static void main(String[] args) {
+		Iniciar inici = new Iniciar();
+		inici.iniciar_exercici();
+		}
+}
--- Level_01/src/lvl1_exercise_01/classes/Month.java
+++ Level_01/src/lvl1_exercise_01/classes/Month.java
@@ -1,4 +1,4 @@
-package lvl1_exercise_01;
+package lvl1_exercise_01.classes;
 
 public class Month {
 
